9547c7a8747e98a2c9da9ae2c05e771a40106afd
val-0715 <valeriem97@nycstudents.net>
2025-12-11 13:46:30 +0000

    comments and imports

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # hi
+
+# IMPORTS - Valerie
+
+# OPTIONS AND INPUTS - Alvin
+
+# WORD COMPONENTS - Arshia
+
+# SAVING AND NAMING - Valerie
+
+from docx import Paragraph
+from docx.shared import Pt
+
+
+
