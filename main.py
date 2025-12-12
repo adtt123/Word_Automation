683395c9bba1d7f60146a677ddd8e47ad3109694
Alvin <Alv1nC03111219@gmail.com>
2025-12-12 14:01:54 +0000

    afsaekfnewoifseofiufae

--- main.py
+++ main.py
@@ -5,8 +5,21 @@ from docx import Paragraph
 from docx.shared import Pt
 
 # OPTIONS AND INPUTS - Alvin
+name = input("Enter your Name: ")
+job_title = input("Enter your Job Title: ")
+date = input("Enter todays Date (MM/DD/YYYY): ")
+room_number = input("Enter your Room Number: ")
 
+department = input("Select Department (1 = HR, 2 = IT, 3 = Finance): ")
 
+if department == "1":
+    department_name = "HR"
+elif department == "2":
+    department_name = "IT"
+elif department == "3":
+    department_name = "Finance"
+else:
+    raise ValueError("Invalid option. Please enter 1, 2, or 3.")
 # WORD COMPONENTS - Arshia
 
 
