ba978485fde2ae4d8954f4f12fd6ed3ead66fd62
cynophilist <84233234+adtt123@users.noreply.github.com>
2025-12-12 13:25:41 +0000

    Edited format

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 # hi
 
 # IMPORTS - Valerie
-
-# OPTIONS AND INPUTS - Alvin
-
-# WORD COMPONENTS - Arshia
-
-# SAVING AND NAMING - Valerie
-
 from docx import Paragraph
 from docx.shared import Pt
 
+# OPTIONS AND INPUTS - Alvin
 
 
+# WORD COMPONENTS - Arshia
+
+
+# SAVING AND NAMING - Valerie
